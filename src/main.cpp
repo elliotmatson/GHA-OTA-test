74b8a062b9eb9d0286fa3b0c878ad3ce9cb107ce
Elliot Matson <ematson5897@gmail.com>
2022-06-14 22:30:21 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <HTTPClient.h>
+#include <HTTPUpdate.h>
+
+#define CHECK_FOR_UPDATES_INTERVAL 5
+#define VERSION "0.0.0"
+
+unsigned long getUptimeSeconds();
+void firmwareUpdate();
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin();
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  // ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  firmwareUpdate();
+}
+
+void firmwareUpdate()
+{
+    static unsigned long lastFirmwareCheck = 0;
+    unsigned long uptime = getUptimeSeconds();
+    if (uptime - lastFirmwareCheck < CHECK_FOR_UPDATES_INTERVAL)
+        return;
+
+    lastFirmwareCheck = uptime;
+
+    HTTPClient http;
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    String firmwareUrl = "https://github.com/elliotmatson/GHA-OTA-test/releases/latest/download/esp32.bin";
+    if (!http.begin(client, firmwareUrl))
+        return;
+
+    int httpCode = http.sendRequest("HEAD");
+    if (httpCode < 300 || httpCode > 400 || http.getLocation().indexOf(String(VERSION)) > 0)
+    {
+        Serial.printf("Not updating from (sc=%d): %s\n", httpCode, http.getLocation().c_str());
+        http.end();
+        return;
+    }
+    else
+    {
+        Serial.printf("Updating from (sc=%d): %s\n", httpCode, http.getLocation().c_str());
+    }
+
+    httpUpdate.setFollowRedirects(HTTPC_FORCE_FOLLOW_REDIRECTS);
+    t_httpUpdate_return ret = httpUpdate.update(client, firmwareUrl);
+
+    switch (ret)
+    {
+    case HTTP_UPDATE_FAILED:
+        Serial.printf("Http Update Failed (Error=%d): %s\n", httpUpdate.getLastError(), httpUpdate.getLastErrorString().c_str());
+        break;
+
+    case HTTP_UPDATE_NO_UPDATES:
+        Serial.printf("No Update!\n");
+        break;
+
+    case HTTP_UPDATE_OK:
+        Serial.printf("Update OK!\n");
+        break;
+    }
+}
+
+unsigned long getUptimeSeconds()
+{
+    return (unsigned long)(esp_timer_get_time() / 1000000ULL);
+}
