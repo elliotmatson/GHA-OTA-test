b2b12660a77b5265d9a5b25f793432edd1c3e2e3
Elliot Matson <ematson5897@gmail.com>
2022-06-18 19:31:22 -0500

    slower fade

--- src/main.cpp
+++ src/main.cpp
@@ -54,11 +54,11 @@ void setup() {
 void loop() {
   for(int i=0; i<255; i++) {
     analogWrite(2, i);
-    delay(10);
+    delay(5);
   }
   for(int i=255; i>=0; i--) {
     analogWrite(2, i);
-    delay(10);
+    delay(5);
   }
 }
 
