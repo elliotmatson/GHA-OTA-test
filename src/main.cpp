98b4bd0edc2400cda684be8c093532d5966eda91
Elliot Matson <ematson5897@gmail.com>
2022-06-18 19:27:08 -0500

    slow fade

--- src/main.cpp
+++ src/main.cpp
@@ -52,10 +52,14 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(100);
-  digitalWrite(2, LOW);
-  delay(100);
+  for(int i=0; i<255; i++) {
+    analogWrite(2, i);
+    delay(10);
+  }
+  for(int i=255; i>=0; i--) {
+    analogWrite(2, i);
+    delay(10);
+  }
 }
 
 void checkForUpdates(void * parameter){
